f003b323362547f6b0f44610c36a1b326031f75b
Muy <lzqm04@gmail.com>
2024-06-12 20:28:06 +0800

    使用Spring Task定时处理超时/未处理订单

---
+++ src/sky-take-out/sky-server/src/main/java/com/sky/task/OrderTask.java
@@ -0,0 +1,77 @@
+package com.sky.task;
+
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.sky.entity.Orders;
+import com.sky.mapper.OrdersMapper;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.Resource;
+import java.time.LocalDateTime;
+import java.util.List;
+
+/**
+ * 定时任务类，定时处理超时订单
+ */
+
+@Component
+@Slf4j
+public class OrderTask {
+
+    @Resource
+    OrdersMapper ordersMapper;
+
+
+    /**
+     * 处理超时未支付订单
+     */
+    //@Scheduled(cron = "0 * * * * ?")    //每分钟触发一次
+    @Scheduled(cron = "0/5 * * * * ?")    //每分钟触发一次
+    public void processTimeoutOrder() {
+        log.info("定时处理超时订单：{}", LocalDateTime.now());
+
+        LocalDateTime time = LocalDateTime.now().plusMinutes(-15);
+
+        LambdaQueryWrapper<Orders> qw = new LambdaQueryWrapper<>();
+        qw.eq(Orders::getStatus, Orders.PENDING_PAYMENT)
+                .lt(Orders::getOrderTime, time);
+
+        List<Orders> ordersList = ordersMapper.selectList(qw);
+
+        if (!ordersList.isEmpty()) {
+            for (Orders orders : ordersList) {
+                orders.setStatus(Orders.CANCELLED);
+                orders.setCancelReason("订单超时，自动取消");
+                orders.setCancelTime(LocalDateTime.now());
+                ordersMapper.update(orders);
+            }
+        }
+    }
+
+    /**
+     * 处理一直处于运输中的订单
+     */
+    @Scheduled(cron = "0 0 1 * * ?")    //每天凌晨一点触发
+    public void processDeliverOrder(){
+        log.info("定时处理处于派送中的订单：{}", LocalDateTime.now());
+
+        LocalDateTime time = LocalDateTime.now().plusMinutes(-60);
+
+        LambdaQueryWrapper<Orders> qw = new LambdaQueryWrapper<>();
+        qw.eq(Orders::getStatus, Orders.DELIVERY_IN_PROGRESS)
+                .lt(Orders::getDeliveryTime, time);
+
+        List<Orders> ordersList = ordersMapper.selectList(qw);
+
+        if (!ordersList.isEmpty()) {
+            for (Orders orders : ordersList) {
+                orders.setStatus(Orders.CANCELLED);
+                orders.setCancelReason("订单超时，自动取消");
+                orders.setCancelTime(LocalDateTime.now());
+                ordersMapper.update(orders);
+            }
+        }
+
+    }
+}
